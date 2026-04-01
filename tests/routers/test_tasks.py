ad97eb8f0e2d0197241022555ea865be509c5245
Jacques Massey <jacques.massey@outlook.com>
2026-04-01 15:50:02 +0100

    Update tests for PUT and DELETE

--- tests/routers/test_tasks.py
+++ tests/routers/test_tasks.py
@@ -11,18 +11,18 @@ def test_get_tasks_unauthenticated(client):
     assert response.status_code == 401
 
 def test_get_tasks_authenticated(client, test_user, auth_headers_for_user):
-    auth_headers = auth_headers_for_user(test_user)
-    response = client.get("/tasks/", headers=auth_headers)
+    headers = auth_headers_for_user(test_user)
+    response = client.get("/tasks/", headers=headers)
     assert response.status_code == 200
 
 def test_can_access_own_task(client, auth_headers_for_user, test_user, test_user_task):
-    auth_headers = auth_headers_for_user(test_user)
-    response = client.get(f"/tasks/{test_user_task.id}", headers=auth_headers)
+    headers = auth_headers_for_user(test_user)
+    response = client.get(f"/tasks/{test_user_task.id}", headers=headers)
     assert response.status_code == 200
 
 def test_cannot_access_other_users_tasks(client, auth_headers_for_user, test_user, other_user_task):
-    auth_headers = auth_headers_for_user(test_user)
-    response = client.get(f"/tasks/{other_user_task.id}", headers=auth_headers)
+    headers = auth_headers_for_user(test_user)
+    response = client.get(f"/tasks/{other_user_task.id}", headers=headers)
     assert response.status_code == 403
 
 def test_unauthenticated_single_task(client, other_user_task):
@@ -30,21 +30,50 @@ def test_unauthenticated_single_task(client, other_user_task):
     assert response.status_code == 401
 
 def test_access_task_does_not_exist(client, auth_headers_for_user, test_user):
-    auth_headers = auth_headers_for_user(test_user)
-    response = client.get("/tasks/9999999", headers=auth_headers)
+    headers = auth_headers_for_user(test_user)
+    response = client.get("/tasks/9999999", headers=headers)
     assert response.status_code == 404
 
 def test_create_task(client, auth_headers_for_user, test_user):
     data = {"title": "New task"}
-    auth_headers = auth_headers_for_user(test_user)
-    response = client.post("/tasks/", json=data, headers=auth_headers)
+    headers = auth_headers_for_user(test_user)
+    response = client.post("/tasks/", json=data, headers=headers)
     assert response.status_code == 200
     assert response.json()["title"] == "New task"
 
 def test_create_task_sets_owner(client, auth_headers_for_user, test_user):
     data = {"title": "Owned task"}
-    auth_headers = auth_headers_for_user(test_user)
-    response = client.post("/tasks/", json=data, headers=auth_headers)
+    headers = auth_headers_for_user(test_user)
+    response = client.post("/tasks/", json=data, headers=headers)
     assert response.status_code == 200
-    # body = response.json()
-    # assert "user_id" in body
+    body = response.json()
+    assert "user_id" in body
+
+def test_update_own_task(client, auth_headers_for_user, test_user, test_user_task):
+    data = {"title": "Updated title", "completed": True}
+    auth_headers = auth_headers_for_user(test_user)
+    response = client.put(f"/tasks/{test_user_task.id}", json=data, headers=auth_headers)
+    assert response.status_code == 200
+    assert response.json()['title'] == "Updated title"
+
+def test_update_others_task(client, auth_headers_for_user, test_user, other_user_task):
+    data = {"title": "Updated title", "completed": True}
+    headers = auth_headers_for_user(test_user)
+    response = client.put(f"/tasks/{other_user_task.id}", json=data, headers=headers)
+    assert response.status_code == 403
+
+def test_delete_own_task(client, auth_headers_for_user, test_user, test_user_task):
+    headers = auth_headers_for_user(test_user)
+    response = client.delete(f"/tasks/{test_user_task.id}", headers=headers)
+    assert response.status_code == 200
+
+def test_delete_others_task(client, auth_headers_for_user, test_user, other_user_task):
+    headers = auth_headers_for_user(test_user)
+    response = client.delete(f"/tasks/{other_user_task.id}", headers=headers)
+    assert response.status_code == 403
+
+def test_delete_removes_task(client, auth_headers_for_user, test_user, test_user_task):
+    headers = auth_headers_for_user(test_user)
+    client.delete(f"/tasks/{test_user_task.id}", headers=headers)
+    response = client.get(f"tasks/{test_user_task.id}", headers=headers)
+    assert response.status_code == 404
